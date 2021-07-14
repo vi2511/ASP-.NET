d820d197535615b839890546cbaade877ff5fffb
Vinicius Bistao <vinicius.martins23@fatec.sp.gov.br>
2021-07-14 10:38:49 -0300

    initial commit

---
+++ MinhaDemoMVC/MinhaDemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinhaDemoMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaDemoMVC.Controllers
+{
+    [Route("")] // Rotas por atributos/Sobrecarga
+    [Route("gestao")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
+        public IActionResult Index(string id, string categoria)
+        {
+            return View();
+        }
+        [Route("privacidade")]
+        [Route("politica-de-pravicidade")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
